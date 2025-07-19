d784bbd367fd5448f7aa0862a3cbcba2e83fc78c
threee-hub <55645132+threee-hub@users.noreply.github.com>
2025-07-19 15:44:07 +0800

    fix: fix matched

--- src/components/view.js
+++ src/components/view.js
@@ -67,6 +67,9 @@ export default {
     // cache component
     cache[name] = { component }
 
+    // attach the route record to the vnode data for use in hooks
+    data.routerViewMatched = matched
+
     // attach instance registration hook
     // this will be called in the instance's injected lifecycle hooks
     data.registerRouteInstance = (vm, val) => {
@@ -82,8 +85,25 @@ export default {
 
     // also register instance in prepatch hook
     // in case the same component instance is reused across different routes
-    ;(data.hook || (data.hook = {})).prepatch = (_, vnode) => {
-      matched.instances[name] = vnode.componentInstance
+    ;(data.hook || (data.hook = {})).prepatch = (oldVnode, vnode) => {
+      const oldMatched = oldVnode.data.routerViewMatched
+      const newMatched = vnode.data.routerViewMatched
+      if (newMatched && oldMatched && newMatched.components[name] !== oldMatched.components[name]) {
+        // an upcoming patch will replace the component, leave it to the teardown logic
+        return
+      }
+      if (newMatched) {
+        // update the instance reference on the new matched record
+        newMatched.instances[name] = vnode.componentInstance
+
+        // and clear the reference on the old record
+        if (oldMatched && oldMatched !== newMatched) {
+          oldMatched.instances[name] = undefined
+        }
+      } else if (oldMatched) {
+        // clearing the instance reference
+        oldMatched.instances[name] = undefined
+      }
     }
 
     // register instance in init hook
